80f7ba504297e087a1c028e1ffc962d273abc0ff
gtaylor90 <graham.taylor.90@gmail.com>
2016-06-27 00:39:41 -0500

    route done, need to finish up the models and views

--- js/script.js
+++ js/script.js
@@ -5,47 +5,70 @@
 // https://openapi.etsy.com/v2/listings/active.js?keywords="+terms+"&limit=12&includes=Images:1&api_key="+api_key
 
 var qs = function(input) {
-	return document.querySelector(input)
+    return document.querySelector(input)
 }
-var ItemCollection = Backbone.Collection.$.extend({
-	url: "https://openapi.etsy.com/v2/listings/active"
-	_key: "r0qoq1mdfhswufpc5krm18n4"
-	parse: function(rawJSON){
-		return rawJSON.response.docs
-	}
+var ItemCollection = Backbone.Collection.extend({
+    url: "https://openapi.etsy.com/v2/listings/active",
+    _key: "r0qoq1mdfhswufpc5krm18n4",
+    parse: function(rawJSON) {
+        return rawJSON.response.docs
+    }
 });
 
 
-var EtsyRoutes = Backbone.Router.extend({
-	routes: {
-		"/store/:shopID/:itemId": "doItemView",
-		"/search/:topic": "doSearchView", 
-		"/home": "showHomeView", 
-		"*catchAll": "redirect" 
-	},
-	doItemView: function(itemID) {
+var EtsyRouter = Backbone.Router.extend({
+    routes: {
+        "/store/:shopID/:itemId": "doItemView",
+        "/search/:topic": "doSearchView",
+        "/home": "showHomeView",
+        "*catchAll": "redirect"
+    },
+    doItemView: function(itemID) {
+    	var itemModel = new ItemCollection()
+    	console.log(itemModel)
+    	itemModel.fetch({
+    		dataType: 'jsonp',
+    		processData: true,
+    		data: {
+    			apikey: itemModel._key
+    		}
+    	})
+    },
+    doSearchView: function(searchTerm) {
+        var searchCollection = new ItemCollection()
+        searchCollection.fetch({
+            dataType: 'jsonp',
+            processData: true,
+            data: {
 
-	},
-	doSearchView: function(searchTerm) {
-		var searchCollection = new ItemCollection()
-		searchCollection.fetch({
-			dataType: 'jsonp',
-			processData: true,
-			data: {
+            }
 
-			}
+        });
+        new ItemsView(searchCollection);
+    },
+    showHomeView: function() {
+        var homeCollection = new ItemCollection()
+        homeCollection.fetch({
+            dataType: 'jsonp',
+            processData: true,
+            data: {
 
-		});
-		new ItemsView(searchCollection);
-	},
-	showHomeView: function() {
+            }
+        })
 
-	},
-	redirect: function() {
-
-	},
-	initialize: function(){
-		Backbone.history.start();
-	}
+    },
+    redirect: function() {
+    	location.hash = "home"
+    },
+    initialize: function() {
+        Backbone.history.start();
+    }
 });
 
+new EtsyRouter()
+
+qs('input').addEventListener('keydown', function(e) {
+    if (e.keyCode === 13) {
+        location.hash = "search/" + e.target.value
+    }
+})
